8cade8ee497ceb6e07bd4d6e16d9d91f403e9228
3axap4eHko <3axap4eHko@gmail.com>
2017-11-04 20:54:00 -0400

    fix main file

---
+++ index.js
@@ -0,0 +1,9 @@
+const Schema = require('./libs/Schema');
+const Model = require('./libs/Model');
+const RDB = require('./libs/RDB');
+
+module.exports = {
+  Schema,
+  Model,
+  RDB
+};
--- libs/index.js
+++
@@ -1,9 +0,0 @@
-const Schema = require('./Schema');
-const Model = require('./Model');
-const RDB = require('./RDB');
-
-module.exports = {
-  Schema,
-  Model,
-  RDB
-};
